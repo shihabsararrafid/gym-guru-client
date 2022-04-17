c39e6a13126ed0ef485c256b2e45f4389d2aabf2
shiabsararrafid <shrafid.532@gmail.com>
2022-04-17 10:26:10 +0600

    header responsive and hambuger menu added

--- src/Components/Pages/Shared/Header/Header.js
+++ src/Components/Pages/Shared/Header/Header.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import gymLogo from './../../../../2599735.png';
+import bar from '../../../../images/bars-solid.svg';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className='h-[100px] bg-[#6C09F1] text-white  flex items-center justify-between'>
+        <div className={`h-[100px] ${open ? 'h-[200px]' : 'h-[100px]'} bg-[#6C09F1] text-white  flex items-center justify-between`}>
             <div className="left-nav flex mx-8 items-center">
 
                 <img className='rounded-full w-[80px]' src={gymLogo} alt="" />
-                <h1 className='text-4xl mx-5 font-semibold'>GYM KING</h1>
+                <h1 className='lg:text-4xl text-2xl  mx-5 font-semibold'>GYM KING</h1>
             </div>
-            <nav className='flex w-[50%] text-xl uppercase font-bold justify-around'>
+
+
+
+            <nav className={` lg:flex ${open ? 'flex' : 'hidden'} ${open ? 'flex-col' : 'hidden'} flex-row ${open ? 'w-[70px]' : 'hidden'} lg:w-[50%] text-xl uppercase font-bold justify-around`}>
                 <div className='group'>
                     <Link className='block hover: box-border duration-700 border-black' to="/home">Home</Link>
                     <div className='border-b-2   -translate-x-14  group-hover:translate-x-0  group-hover:border-[#07F3EA] duration-700 border-[#6C09F1]'></div>
@@ -32,12 +39,22 @@ const Header = () => {
                     <Link className='block hover: box-border duration-700 border-black' to="/login">Login</Link>
                     <div className='border-b-2   -translate-x-14  group-hover:translate-x-0  group-hover:border-[#07F3EA] duration-700 border-[#6C09F1]'></div>
                 </div>
+            </nav>
+            <div className='lg:hidden block '>
+                <FontAwesomeIcon onClick={() => setOpen(!open)} className='w-[40px]' icon={faBars}></FontAwesomeIcon>
+            </div>
 
-                {/* <Link to="/services">Services</Link>
+
+
+
+
+            {/* <Link to="/services">Services</Link>
                 <Link to="/about">About</Link>
                 <Link to="/blogs">Blog</Link>
                 <Link to="/login">Login</Link> */}
-            </nav>
+
+
+
         </div>
     );
 };
