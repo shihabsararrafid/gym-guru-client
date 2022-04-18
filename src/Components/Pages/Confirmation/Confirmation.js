e7d425248d1f4de64c1afcf30783a3446ede8a11
shiabsararrafid <shrafid.532@gmail.com>
2022-04-18 10:52:22 +0600

    confirmation msg showed

--- src/Components/Pages/Confirmation/Confirmation.js
+++ src/Components/Pages/Confirmation/Confirmation.js
@@ -1,21 +1,40 @@
 import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 const Confirmation = () => {
+    const [done, setDone] = useState(0);
 
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data);
+        setDone(1);
+    };
+
+    if (done) {
+        setTimeout(() => {
+            const msg = document.getElementById('msg');
+            msg.style.display = "flex";
+        }, [0])
+        setTimeout(() => {
+            const msg = document.getElementById('msg');
+            msg.style.display = "none";
+        }, [3000])
+        setDone(0);
+    }
 
     // watch input value by passing the name of it
 
 
     return (
-        <div className='lg:w-1/2 md:w-3/4 w-full mb-10 mx-auto'>
-            <h1 className='text-3xl text-violet-700 my-10 text-center'>Please provide your detail info</h1>
+        <div className='lg:w-1/2 md:w-3/4 relative w-full mt-0 mb-10 mx-auto'>
+            <div id="msg" className={`border-[1px] mx-auto right-1/2  mt-0 top-0 w-[300px] rounded-lg flex items-center justify-center  border-violet-500`}>
+                <h1 className='bg-violet-700 text-white w-full text-center rounded-lg px-5 py-3  font-bold text-lg'>Thanks For Your Booking</h1>
+            </div>
+            <h1 className='text-3xl text-violet-700 mb-10 mt-16 text-center'>Please provide your detail info</h1>
 
 
-            < form onSubmit={handleSubmit(onSubmit)}>
+            < form className='mt-10' onSubmit={handleSubmit(onSubmit)}>
 
                 {/* User name section with validation */}
                 <h1 className='text-md  font-bold'>Username:</h1>
@@ -48,7 +67,7 @@ const Confirmation = () => {
 
 
                 {/* Submit Button with onsubmit hook function */}
-                <input className='text-center block   w-[90%] ] mx-auto  my-6 text-lg py-4 border-[1px] font-semibold ml-5 border-black hover:text-[#6C05F6] duration-300 hover:border-[#6C05F6] bg-violet-700 hover:bg-white text-white   rounded-full' type="submit" />
+                <input className='text-center block cursor-pointer   w-[90%] ] mx-auto  my-6 text-lg py-4 border-[1px] font-semibold ml-5 border-black hover:text-[#6C05F6] duration-300 hover:border-[#6C05F6] bg-violet-700 hover:bg-white text-white   rounded-full' type="submit" />
 
 
             </form>
